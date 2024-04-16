efcc255e6cd0ce302a4b06186de114e0a690dc0d
RebecaCamilo <rebecacamilo3@gmail.com>
2024-04-16 11:30:42 -0300

    feat: order produtos list by price

--- src/curso3/ComprasDriver.java
+++ src/curso3/ComprasDriver.java
@@ -1,34 +1,60 @@
 package curso3;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class ComprasDriver {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scNumber = new Scanner(System.in);
+        Scanner scString = new Scanner(System.in);
 
         System.out.println("Digite o limite do cartão:");
-        Double limite = sc.nextDouble();
+        Double limite = scNumber.nextDouble();
 
-        Cartao cartao =  new Cartao(limite, new ArrayList<>());
+        Cartao cartao = new Cartao(limite, new ArrayList<>());
 
         int op = 1;
         Double somaCompras = 0.0;
 
-        while (op != 0 && somaCompras < cartao.getLimite()) {
+        while (op != 0) {
             System.out.println("Digite a descrição da compra");
-            String descricao = sc.nextLine();
+            String descricao = scString.nextLine();
             System.out.println("Digite o valor da compra");
-            Double valor = sc.nextDouble();
+            Double valor = scNumber.nextDouble();
 
-            cartao.getProduto().add(new Produto(descricao, valor));
-            somaCompras += valor;
-            System.out.println("Compra realizada!");
+            if ((somaCompras + valor) <= cartao.getLimite()) {
 
-            System.out.println("Digite '0' para sair ou '1' para continuar");
-            op = sc.nextInt();
+                somaCompras += valor;
+
+                cartao.getProdutos().add(new Produto(descricao, valor));
+                System.out.println("Compra realizada!");
+
+                System.out.println("Digite '0' para sair ou '1' para continuar");
+                op = scNumber.nextInt();
+
+            } else {
+
+                System.out.println("Saldo insuficiente!\n");
+
+                op = 0;
+
+            }
         }
 
+        cartao.getProdutos().sort(Comparator.comparing(Produto::getValor));
+
+        System.out.println("***************************");
+        System.out.println("COMPRAS REALIZADAS:\n");
+
+        for (Produto produto : cartao.getProdutos()) {
+            System.out.printf("%s - %.2f\n", produto.getDescricao(), produto.getValor());
+        }
+
+        System.out.println();
+        System.out.println("***************************\n");
+        System.out.println("Saldo do cartão: " + somaCompras);
+
     }
+
 }
