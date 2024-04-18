f546da8190df90ab6e973b5b9f0e4c28a0788f88
RebecaCamilo <rebecacamilo3@gmail.com>
2024-04-18 12:43:15 -0300

    refac: romeve course exercises from main branch

--- src/curso4/via_cep/Endereco.java
+++
@@ -1,8 +0,0 @@
-package curso4.via_cep;
-
-public record Endereco(String cep,
-                       String logradouro,
-                       String bairro,
-                       String localidade,
-                       String uf) {
-}
